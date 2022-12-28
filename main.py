57cb52b30cc392797c579c9fbf0a3ae12ecc77c0
Iago Pontes <iago.pontes@outlook.com>
2022-12-28 17:34:36 -0300

    1.2
    Adicionado função generate_password() e pyperclip.copy para copiar automaticamente a senha gerada.

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import tkinter as tk
 import tkinter.messagebox as mb
+from random import randint, choices, shuffle
+import pyperclip
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = [letter for letter in choices(letters, k=randint(8, 10))]
+    password_list += [symbol for symbol in choices(symbols, k=randint(2, 4))]
+    password_list += [number for number in choices(numbers, k=randint(2, 4))]
+
+    shuffle(password_list)
+    password_list = "".join(password_list)
+    entry_password.delete(0, "end")
+    entry_password.insert(0, password_list)
+    pyperclip.copy(password_list)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -20,7 +37,6 @@ def save():
             with open("data.txt", 'a') as data:
                 data.write(f"{website} | {username} | {password}\n")
             entry_website.delete(0, "end")
-            # entry_username.delete(0, "end")
             entry_password.delete(0, "end")
             entry_website.focus()
 
@@ -60,7 +76,7 @@ entry_password = tk.Entry(width=17)
 entry_password.grid(row=3, column=1)
 
 # Button
-button_generate = tk.Button(text="Generate Password")
+button_generate = tk.Button(text="Generate Password", command=generate_password)
 button_generate.grid(row=3, column=2)
 
 button_add = tk.Button(text="Add", width=30, command=save)
